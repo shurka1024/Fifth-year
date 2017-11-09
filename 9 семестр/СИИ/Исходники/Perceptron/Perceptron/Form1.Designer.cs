0aa1de10570a98d9849225cb428d009f7fc67e8a
shurka1024 <shurka1024@gmail.com>
2017-11-09 16:51:12 +0400

    09.11.2017

---
+++ 9 семестр/СИИ/Исходники/Perceptron/Perceptron/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Perceptron
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.X1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.X2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(863, 462);
+            this.splitContainer1.SplitterDistance = 287;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X1,
+            this.X2,
+            this.Y});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(346, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // X1
+            // 
+            this.X1.Frozen = true;
+            this.X1.HeaderText = "X1";
+            this.X1.Name = "X1";
+            // 
+            // X2
+            // 
+            this.X2.Frozen = true;
+            this.X2.HeaderText = "X2";
+            this.X2.Name = "X2";
+            // 
+            // Y
+            // 
+            this.Y.Frozen = true;
+            this.Y.HeaderText = "Y";
+            this.Y.Name = "Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 462);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Работа №1 \"Персептроны\"";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+    }
+}
+
---
+++ 9 семестр/СИИ/Исходники/Perceptron/Perceptron/Perceptron.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perceptron
+{
+    public class Perceptron
+    {
+        /// <summary>
+        /// Массив входных нейронов
+        /// </summary>
+        private List<double> Enters { get; set; }
+
+        /// <summary>
+        /// Выходной нейрон
+        /// </summary>
+        private double Outer { get; set; }
+
+        /// <summary>
+        /// Весовые коэффициенты
+        /// </summary>
+        private List<double> Weights { get; set; }
+
+        double[,] Patterns = {
+            { 0, 0, 1 },
+            { 0, 1, 0},
+            { 1, 0, 0},
+            { 1, 1, 0}
+        };
+
+        public Perceptron()
+        {
+            Enters = new List<double>();
+            Weights = new List<double>();
+        }
+
+        public void GenerateNewWeights(int count)
+        {
+            Weights.Clear();
+            for (int i = 0; i < count; ++i)
+            {
+                Weights.Add((new Random()).Next());
+            }
+            Weights.Add((new Random()).Next());
+        }
+
+        public void CalculateOuter()
+        {
+            Outer = 0;
+            for (int i = 0; i < Enters.Count; ++i)
+            {
+                Outer += Enters[i] * Weights[i];
+            }
+
+            Outer = Outer > 0.5 ? 1 : 0;
+        }
+
+        public void Study(List<StudyPattern> patterns)
+        {
+            if (patterns == null || !patterns.Any())
+            {
+                return;
+            }                
+
+            double? globalError = null;
+            GenerateNewWeights(patterns.First().StudyEnters.Count);
+            
+            while (globalError != 0)
+            {
+                globalError = 0;
+                for (int p = 0; p < patterns.Count; ++p)
+                {
+                    var studyOut = patterns[p].StudyOut;
+                    Enters.Clear();
+                    Enters.AddRange(patterns[p].StudyEnters);
+
+                    CalculateOuter();
+
+                    var localError = studyOut - Outer;
+                    globalError += Math.Abs(localError);
+
+                    for (int i = 0; i < Enters.Count; ++i)
+                    {
+                        Weights[i] += 0.1 * localError * Enters[i];
+                    }
+                }
+            }
+        }
+
+        public void Test()
+        {
+            var patterns = new List<StudyPattern>();
+            patterns.Add(new StudyPattern { StudyEnters = new List<double> { 0, 0, 1 }, StudyOut = 1 });
+            patterns.Add(new StudyPattern { StudyEnters = new List<double> { 0, 1, 1 }, StudyOut = 0 });
+            patterns.Add(new StudyPattern { StudyEnters = new List<double> { 1, 0, 1 }, StudyOut = 0 });
+            patterns.Add(new StudyPattern { StudyEnters = new List<double> { 1, 1, 1 }, StudyOut = 0 });
+            Study(patterns);
+
+            for (int p = 0; p < patterns.Count; ++p)
+            {
+                Enters.Clear();
+                Enters.AddRange(patterns[p].StudyEnters);
+
+                CalculateOuter();
+            }
+        }
+    }
+
+    public class StudyPattern
+    {
+        public List<double> StudyEnters { get; set; }
+
+        public double StudyOut { get; set; }
+    }
+}
